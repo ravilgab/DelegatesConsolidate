e55bc817f2535c547153a85c101cf616dcdd196f
Ravil <ravilgab@yahoo.com>
2021-10-23 21:44:21 +0300

    Added new ViewController and some outlets

---
+++ TestDelete/ViewControllers/PhotoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoViewController.swift
+//  TestDelete
+//
+//  Created by Ravil on 23.10.2021.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    @IBOutlet weak var roundedPhotoLabel: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
